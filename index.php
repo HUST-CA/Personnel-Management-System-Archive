a2096c4aa642252e8dd1ba4bec0ee25db29b9851
naihaishy <448435279@qq.com>
2016-05-07 12:11:41 +0800

    首页放什么呢？

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php require_once("./common/config.php");?>
+<?php
+$query=mysql_query();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+      <title>PMS--HUSTCA</title>
+      <meta charset="utf-8">
+      <meta http-equiv="Cache-Control" content="no-transform "/>
+      <meta name="keyword" content="华中科技大学计算机协会 HUSTCA"/>
+      <meta name="description" content="HUSTCA 华科计算机 协会 社团 华中科技大学计算机协会 华科社团 hust" />
+      <meta name="viewport" content="width=device-width, initial-scale=1,minimun-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css"/>
+     <link rel="stylesheet" type="text/css" href="style/css/custom.css"/>
+</head>
+
+<body>
+
+<!-- header Secondary menu -->
+<?php require_once("header.php");?>
+
+<!-- Main Page -->
+<div class="container-fluid">
+  <div class="col-md-12"></div>
+  <div class="col-md-12"></div>
+  <div class="col-md-12"></div>
+
+
+</div>
+
+
+<!--footer -->
+<?php require_once("./footer.php");?>
+</body>
+</html>
