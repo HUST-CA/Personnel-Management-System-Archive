860c1073d290c8e703a9f1fb4211b44ac9e5d59a
naihaishy <448435279@qq.com>
2016-05-08 13:47:49 +0800

    2

--- user/update.php
+++ user/update.php
@@ -25,10 +25,10 @@
           <div class="control-group">
             <label class="control-label" for="motto">人生格言</label>
             <div class="controls">
-              <input type="text" id="motto" name="motto" placeholder="写一句激励自己的话吧"  class="span8">
+              <input type="text" id="motto" name="motto" placeholder="人生格言"  class="span8">
             </div>
           </div>
-    
+
           <div class="control-group">
             <div class="controls">
                    <button type="submit" class="btn btn-large btn-primary reg_submit_btn" name="reg_btn">确认</button>
